8cd465b791617e7e37d70928a6c6d19418c57163
SJin Lee <arer90@gmail.com>
2024-05-18 22:54:37 +0900

    version_4_27

---
+++ 2_DSA/cpp/tree/STL_map_and_STL_set.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    set : only save key
+    map : save key and value
+
+    set vs multiset:
+    - set is no duplicate
+    - multiset is allowed to have a duplicated value
+
+    set vs unordered_set:
+    - set is inner data with sorted 
+    - unordered_set is no sorted
+
+    std:set:
+    - key = value
+    - saved date is settled with key value
+    - insert, delete, travesal = O(logn0)
+    - std:set = red-black tree
+    - if u want to have duplicate values, use std::multiset
+    - if u want to unorder sorting array, use std::unordered_set
+    - if u want to data type, it need to compare each other
+    - <set> is already settled up
+
+    functions:
+    begin(), end(),
+    rbegin(), rend(),
+    insert() // emplace()
+    erase()
+    find()  // contains
+    clear()
+    size()
+    empty()
+*/
+
+int main(){
+
+    return 0;
+}
