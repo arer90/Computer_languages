4802d9bc7d1acf2cef97054b10613201481d59e4
SJin Lee <arer90@gmail.com>
2024-06-12 10:02:25 +0900

    version_4_52

--- 2_DSA/python/basic/variables.py
+++ 2_DSA/python/basic/variables.py
@@ -5,8 +5,12 @@ var = ["a","b","c","d","e"]
 for i in range(len(var)):
     print(f"{i} : {var[i]}")
     
-
 dic = {'fruit1':'apple', 'fruit2':'orange', 'fruit3':'banana'}
 
 for k,v in dic.items():
     print(f'{k} is {v}')
+    
+
+setNum = {1,2,3,4,5,5,5,6,7,7,8,9,9,9}
+
+print(f'{setNum}')
